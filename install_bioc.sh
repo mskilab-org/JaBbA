e8b0727ab716acd00253967a155acee5fc850006
Khagay Nagdimov <Crtemois612@gmail.com>
2016-05-26 17:31:39 -0400

    update README.md

--- install_bioc.sh
+++ install_bioc.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-Rscript -e 'source("https://bioconductor.org/biocLite.R"); biocLite("BiocInstaller"); biocLite("BSgenome.Hsapiens.UCSC.hg19"); biocLite("GenomicRanges"); install.packages("devtools"); devtools:: install_github("mskilab/gUtils"); devtools::install_github("jimhester/covr");'
+Rscript -e 'source("https://bioconductor.org/biocLite.R"); biocLite("BiocInstaller"); biocLite("BSgenome.Hsapiens.UCSC.hg19"); biocLite("GenomicRanges"); install.packages("devtools"); devtools:: install_github("mskilab/gUtils"); 
+devtools::install_github("jimhester/covr"); devtools::install_github("mskilab/gTrack");'
