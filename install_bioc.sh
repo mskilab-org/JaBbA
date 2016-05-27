636001f53d66be773cb4d187870607f8fd7f288d
Khagay Nagdimov <khagay@nyu.edu>
2016-05-27 12:58:56 -0400

    edit install_bioc

--- install_bioc.sh
+++ install_bioc.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-Rscript -e 'source("https://bioconductor.org/biocLite.R"); biocLite("BiocInstaller"); biocLite("BSgenome.Hsapiens.UCSC.hg19"); biocLite("GenomicRanges"); install.packages("devtools"); devtools:: install_github("mskilab/gUtils"); 
-devtools::install_github("jimhester/covr"); devtools::install_github("mskilab/gTrack"); library(gTrack);'
+Rscript -e 'source("https://bioconductor.org/biocLite.R"); 
+biocLite("BiocInstaller"); 
+biocLite("BSgenome.Hsapiens.UCSC.hg19"); 
+biocLite("GenomicRanges"); 
+install.packages("devtools"); 
+devtools:: install_github("mskilab/gUtils"); 
+devtools::install_github("jimhester/covr"); 
+devtools::install_github("mskilab/gTrack"); 
+
+library(gTrack);'
