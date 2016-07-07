a0648be3a835a46305475241b66f196f3253edb7
Jeremiah Wala <jwala@broadinstitute.org>
2016-07-07 15:27:12 -0400

    added install / run instructions for CONSERTING

---
+++ other/conserting/run_conserting.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+## modify paths
+CONSERTING_ROOT_DIR=/xchip/gistic/Jeremiah/Projects/Jabba/CONSERTING/code
+export PATH=$PATH:$CONSERTING_ROOT_DIR
+export CLASSPATH=$CLASSPATH:$CONSERTING_ROOT_DIR:$CONSERTING_ROOT_DIR/bambino_bundle_1.06.jar:$CONSERTING_ROOT_DIR/sam-1.65.jar:$CONSERTING_ROOT_DIR/mysql-connector-java-5.1.39.tar.gz
+
+TUM=$1
+NORM=$2
+
+## check that files exist, and link them (stupid bai vs bam.bai issues
+linkr $TUM tum.bam
+linkr $NORM norm.bam
+
+## convert BAM to WIG
+time sh bam2wig.sh tum.bam tum.wig
+time sh bam2wig.sh norm.bam norm.wig
+
+## function for symlinking BAMs
+function linkr() {
+   
+    if [[ ! -e $1 ]]; then
+	echo "BAM file: $1 does not exists. Exiting"
+	exit 1
+    fi
+
+    ## find BAM index, whether .bai or .bam.bai
+    BAI=`echo ${1} | sed s/bam$/bai/`
+    if [[ ! -e $BAI ]]; then
+	BAI="${TUM}.bai"
+    fi
+    
+    ## check that BAM index exists
+    if [[ ! -e $BAI ]]; then
+	echo "BAM index file: $BAI does not exist. Exiting"
+	exit 1
+    fi
+
+    ## symlink with naming convention required for conserting
+    ln -s ${1} ${2} 
+    ln -s $BAI ${2}.bai
+} 
