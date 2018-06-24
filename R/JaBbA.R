728c098f89ac5972ac7866b1cb1998690f42ba96
Xiaotong Yao <xiaotong.yao23@gmail.com>
2018-06-23 23:51:37 -0400

    try removing examples

--- R/JaBbA.R
+++ R/JaBbA.R
@@ -95,16 +95,6 @@
 #' @param init jabba object (list) or path to .rds file containing previous jabba object which to use to initialize solution, this object needs to have the identical aberrant junctions as the current jabba object (but may have different segments and loose ends, i.e. is from a previous iteration)
 #' @return gGraph (gGnome package) of balanced rearrangement graph
 #' 
-#' @examples
-#'\dontrun{
-#' library(JaBbA)
-#' junctions = system.file("extdata", "junctions.vcf", package = 'JaBbA')
-#' coverage = system.file("extdata", "coverage.txt", package = 'JaBbA')
-#' 
-#' ## run analysis without hets 
-#' jab = JaBbA(junctions = junctions, coverage = coverage)
-#'}
-#' @import DNAcopy
 #' @export
 JaBbA = function(junctions, # path to junction VCF file, dRanger txt file or rds of GRangesList of junctions (with strands oriented pointing AWAY from breakpoint)
                  junctions.unfiltered = NULL, 
