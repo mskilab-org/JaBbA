8fce5bd141efe755e60f21474e83761ba641f8ca
Marcin Imielinski <mimielinski@nygenome.org>
2018-07-03 23:55:36 -0400

    msg:getting more verbose

--- tests/testthat/test_JaBbA.R
+++ tests/testthat/test_JaBbA.R
@@ -10,6 +10,8 @@ coverage = system.file("extdata", "coverage.txt", package = 'JaBbA')
 hets = system.file("extdata", "hets.txt", package = 'JaBbA')
 segs = system.file("extdata", "segs.rds", package = 'JaBbA')
 
+system('rm -rf JaBbA')
+
 test_that("read.junctions", {
 	expect_equal(all(values(read.junctions(junctions))$tier==c(2, 3, 2, 3, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 3, 2, 3, 3, 3, 3, 3, 3, 3, 2, 2, 3, 3, 2)), TRUE)
   expect_equal(as.data.table(unlist(read.junctions(junctions))[, c()]), as.data.table(unlist(read.junctions(bedpe))[, c()]))
@@ -23,13 +25,13 @@ test_that("reciprocal.cycles", {
 
 set.seed(42);
 TILIM = 60
-jab = JaBbA(junctions = junctions, coverage = coverage, seg = segs, slack.penalty = 1e4, hets = hets, tilim = TILIM, verbose = 1, overwrite = TRUE, ploidy=3.72, purity=NA)
+jab = JaBbA(junctions = junctions, coverage = coverage, seg = segs, slack.penalty = 1e4, hets = hets, tilim = TILIM, verbose = 2, overwrite = TRUE, ploidy=3.72, purity=NA)
 jab2 = readRDS('JaBbA/jabba.rds')
-jab.reiterate = JaBbA(junctions = junctions, coverage = coverage, seg = segs, slack.penalty = 1e4, tilim = TILIM, verbose = 1, overwrite = TRUE, reiterate=3, ploidy=3.72, purity=0.99)  ## reiterate > 1
+jab.reiterate = JaBbA(junctions = junctions, coverage = coverage, seg = segs, slack.penalty = 1e4, tilim = TILIM, verbose = 2, overwrite = TRUE, reiterate=3, ploidy=3.72, purity=0.99)  ## reiterate > 1
 jab.reiterate2 = readRDS('JaBbA/jabba.rds')
 
 print(table(readRDS('JaBbA/karyograph.rds')$segstats$cn))
-print(table(readRDS('JaBbA/jabba.raw.rds')$segstats$cn))
+print(table(round(readRDS('JaBbA/jabba.raw.rds')$segstats$cn)))
 print(table(readRDS('JaBbA/jabba.simple.rds')$segstats$cn))
 
 print('ha')
