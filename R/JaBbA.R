b985310aa639dc61a2459dc0fe7034d7f782efaa
xtYao <xiaotong.yao@nyu.edu>
2020-10-04 15:46:45 -0400

    clean up NAMESPACE

--- R/JaBbA.R
+++ R/JaBbA.R
@@ -1,7 +1,9 @@
-#' @import GenomicRanges
-#' @import gUtils
-#' @import gTrack
-#' @import gGnome
+#' @importFrom gUtils streduce si2gr seg2gr rrbind ra.overlaps ra.duplicated parse.gr hg_seqlengths grl.unlist grl.pivot grl.in grl.eval grl.bind grbind gr2dt gr.val gr.tile.map gr.tile
+#' @importFrom gUtils gr.stripstrand gr.sum gr.string gr.start gr.end gr.simplify gr.setdiff gr.sample gr.reduce gr.rand gr.quantile gr.nochr
+#' @importFrom gUtils gr.match gr.in gr.flipstrand gr.fix gr.findoverlaps gr.duplicated gr.dist gr.disjoin gr.breaks dt2gr "%^%" "%Q%" "%&%" "%$%"
+#' @importFrom gGnome gG
+#' @importFrom GenomicRanges GRanges GRangesList values split match
+#' @importFrom gTrack gTrack
 #' @importFrom igraph graph induced.subgraph V E graph.adjacency clusters
 #' @importFrom optparse make_option OptionParser parse_args print_help
 #' @importFrom data.table data.table as.data.table setnames setkeyv fread setkey
@@ -10,12 +12,13 @@
 #' @importFrom gplots col2hex
 #' @importFrom graphics plot abline hist title
 #' @importFrom grDevices col2rgb dev.off pdf png rgb
-#' @importFrom stats C aggregate dist loess median ppois predict runif setNames hclust cutree
+#' @importFrom stats C aggregate dist loess median ppois predict runif setNames hclust cutree acf glm ks.test lag quantile
 #' @importFrom utils read.delim write.table
 #' @importFrom sequenza segment.breaks baf.model.fit get.ci
 #' @importFrom rtracklayer import
 #' @importFrom GenomeInfoDb seqlengths
 #' @importFrom DNAcopy CNA segment smooth.CNA
+#' @importFrom methods as is
 #' @useDynLib JaBbA
 
 ## appease R CMD CHECK misunderstanding of data.table syntax by declaring these global variables
@@ -595,7 +598,6 @@ JaBbA = function(## Two required inputs
 #' @param slack.penalty  penalty to put on every loose.end copy, should be calibrated with respect to 1/(k*sd)^2 for each segment, i.e. that we are comfortable with junction balance constraints introducing k copy number deviation from a segments MLE copy number assignment (the assignment in the absence of junction balance constraints)
 #' @param init jabba object (list) or path to .rds file containing previous jabba object which to use to initialize solution, this object needs to have the identical aberrant junctions as the current jabba object (but may have different segments and loose ends, i.e. is from a previous iteration)
 #' @param overwrite  flag whether to overwrite existing output directory contents or just continue with existing files.
-#' @import DNAcopy
 jabba_stub = function(junctions, # path to junction VCF file, dRanger txt file or rds of GRangesList of junctions (with strands oriented pointing AWAY from breakpoint)
                       coverage, # path to cov file, rds of GRanges
                       blacklist.coverage = NULL,
@@ -5611,8 +5613,7 @@ convex.basis = function(A, interval = 80, chunksize = 100, maxchunks = Inf,
 #' @return a \code{GRangesList} of the junctions
 #'
 #' @importFrom VariantAnnotation readVcf info geno
-#' @import data.table
-#'
+#' 
 #' @export
 ###########################################################
 read.junctions = function(rafile,
