c5f6896192dccaa2f1f7361ae1bdba7b90317593
Zi-Ning Choo <zining01@gmail.com>
2021-11-01 19:47:01 -0400

    add tests for filter loose flag

---
+++ tests/testthat/test_filterloose.R
@@ -0,0 +1,64 @@
+library(JaBbA)
+library(gUtils)
+library(gGnome)
+library(testthat)
+
+jj = system.file("testing", "junctions.rds", package = "JaBbA")
+sg = system.file("testing", "segs.rds", package = "JaBbA")
+cf = system.file("testing", "coverage.txt", package = "JaBbA")
+
+test_that(desc = "Testing LP JaBbA with .bw coverage input",
+          code = {
+              expect_message({
+                  jba.lp = suppressWarnings(JaBbA(
+                      junctions = jj,
+                      coverage = cf,
+                      seg = sg,
+                      slack.penalty = 10,
+                      tilim = 60,
+                      cfield = 'ratio',
+                      verbose = 2,
+                      outdir = 'JaBbA.filter',
+                      overwrite = TRUE,
+                      ploidy=4.5,
+                      purity=1,
+                      epgap = 0.01,
+                      all.in = TRUE,
+                      tfield = NULL,
+                      nudge.balanced = TRUE,
+                      dyn.tuning = TRUE,
+                      lp = TRUE,
+                      ism = FALSE,
+                      max.na = 1,
+                      filter_loose = TRUE))
+                  },
+                  regexp = "Starting loose end annotation")
+          })
+
+test_that(desc = "Testing LP JaBbA with .bw coverage input",
+          code = {
+              expect_message({
+                  jba.lp = suppressWarnings(JaBbA(
+                      junctions = jj,
+                      coverage = cf,
+                      seg = sg,
+                      slack.penalty = 10,
+                      tilim = 60,
+                      cfield = 'ratio',
+                      verbose = 2,
+                      outdir = 'JaBbA.nofilter',
+                      overwrite = TRUE,
+                      ploidy=4.5,
+                      purity=1,
+                      epgap = 0.01,
+                      all.in = TRUE,
+                      tfield = NULL,
+                      nudge.balanced = TRUE,
+                      dyn.tuning = TRUE,
+                      lp = TRUE,
+                      ism = FALSE,
+                      max.na = 1))
+                  },
+                  regexp = "Skipping loose end annotation")
+          })
+              
